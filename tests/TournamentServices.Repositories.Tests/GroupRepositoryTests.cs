8d59594f6cd28a54a06634de6f275e6af06a1d2e
Efrain012 <efrain.dominguez012@gmail.com>
2026-09-14 22:59:38 +0000

    cosa x sin importancia

--- tests/TournamentServices.Repositories.Tests/GroupRepositoryTests.cs
+++ tests/TournamentServices.Repositories.Tests/GroupRepositoryTests.cs
@@ -1,10 +1,42 @@
-﻿namespace TournamentServices.Repositories.Tests;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit; 
+using TournamentServices.Domain;
+using TournamentServices.Repositories;
+using TournamentServices.TestBuilders; // Importamos los builders de tu equipo
 
-public class UnitTest1
+namespace TournamentServices.Repositories.Tests
 {
-    [Fact]
-    public void Test1()
+    public class GroupRepositoryTests
     {
+        // Cumplimos la regla: un nombre de DB único por test para aislamiento
+        private TournamentDbContext CreateInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<TournamentDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
 
+            return new TournamentDbContext(options);
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldSaveGroupInDatabase()
+        {
+            // Arrange
+            using var context = CreateInMemoryDbContext();
+            var repository = new GroupRepository(context);
+            
+            // Usamos el builder predeterminado
+            var group = new GroupBuilder().Build();
+
+            // Act
+            await repository.AddAsync(group);
+
+            // Assert
+            var savedGroup = await context.Groups.FirstOrDefaultAsync(g => g.Id == group.Id);
+            Assert.NotNull(savedGroup);
+            Assert.Equal(group.Name, savedGroup.Name);
+        }
     }
 }
